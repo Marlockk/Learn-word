b45006c7bfbfb7985a4c66a091817be6df27b58e
Marlockk <valeriy_valeriy322@mail.ru>
2025-08-26 13:05:31 +0500

    Добавлен метод getNextQuestion, теперь работает через LiveData

--- app/src/main/java/com/example/newapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newapp/presentation/MainActivity.kt
@@ -11,7 +11,6 @@ import com.example.newapp.domain.NUMBER_OF_ANSWERS
 import com.example.newapp.R
 import com.example.newapp.data.WordRepository
 import com.example.newapp.databinding.ActivityLearnWordBinding
-import com.example.newapp.domain.QuestionModel
 import com.example.newapp.domain.QuizeInteractor
 
 class MainActivity : AppCompatActivity() {
@@ -58,201 +57,213 @@ class MainActivity : AppCompatActivity() {
 
     fun subscribe() {
         myViewModel?.answer?.observe(this) { isCorrect ->
-            when (isCorrect) {
-                true -> {
-                    markAnswerCorrect(binding.layoutAnswer1, binding.tvVariantNumber1, binding.tvVariantValue1)
-                    markAnswerCorrect(binding.layoutAnswer2, binding.tvVariantNumber2, binding.tvVariantValue2)
-                    markAnswerCorrect(binding.layoutAnswer3, binding.tvVariantNumber3, binding.tvVariantValue3)
-                    markAnswerCorrect(binding.layoutAnswer4, binding.tvVariantNumber4, binding.tvVariantValue4)
-                    showResultMessage(true)
-                }
-                false -> {
-                    markAnswerWrong(binding.layoutAnswer1, binding.tvVariantNumber1, binding.tvVariantValue1)
-                    markAnswerWrong(binding.layoutAnswer2, binding.tvVariantNumber2, binding.tvVariantValue2)
-                    markAnswerWrong(binding.layoutAnswer3, binding.tvVariantNumber3, binding.tvVariantValue3)
-                    markAnswerWrong(binding.layoutAnswer4, binding.tvVariantNumber4, binding.tvVariantValue4)
-                    showResultMessage(false)
+            with(binding) {
+                when (isCorrect) {
+                    true -> {
+                        markAnswerCorrect(layoutAnswer1, tvVariantNumber1, tvVariantValue1)
+                        markAnswerCorrect(layoutAnswer2, tvVariantNumber2, tvVariantValue2)
+                        markAnswerCorrect(
+                            layoutAnswer3,
+                            binding.tvVariantNumber3,
+                            binding.tvVariantValue3
+                        )
+                        markAnswerCorrect(layoutAnswer4, tvVariantNumber4, tvVariantValue4)
+                        showResultMessage(true)
+                    }
+
+                    false -> {
+                        markAnswerWrong(layoutAnswer1, tvVariantNumber1, tvVariantValue1)
+                        markAnswerWrong(layoutAnswer2, tvVariantNumber2, tvVariantValue2)
+                        markAnswerWrong(layoutAnswer3, tvVariantNumber3, tvVariantValue3)
+                        markAnswerWrong(layoutAnswer4, tvVariantNumber4, tvVariantValue4)
+                        showResultMessage(false)
+                    }
                 }
             }
-        }
-    }
-
-
-
-    private fun showNextQuestion() {
-
-                val firstQuestion: QuestionModel? = myViewModel.getNextQuestion()
+            myViewModel?.question?.observe(this) { question ->
                 with(binding) {
-
-                    if (firstQuestion == null || firstQuestion.variants.size < NUMBER_OF_ANSWERS) {
+                    if (question == null || question.variants.size < NUMBER_OF_ANSWERS) {
                         tvQuestionWord.isVisible = false
                         layoutVariants.isVisible = false
                         btnSkip.text = "Complete"
                     } else {
                         btnSkip.isVisible = true
                         tvQuestionWord.isVisible = true
-                        tvQuestionWord.text = firstQuestion.question.original
+                        tvQuestionWord.text = question.question.original
 
-                        tvVariantValue1.text = firstQuestion.variants[0].translate
-                        tvVariantValue2.text = firstQuestion.variants[1].translate
-                        tvVariantValue3.text = firstQuestion.variants[2].translate
-                        tvVariantValue4.text = firstQuestion.variants[3].translate
+                        tvVariantValue1.text = question.variants[0].translate
+                        tvVariantValue2.text = question.variants[1].translate
+                        tvVariantValue3.text = question.variants[2].translate
+                        tvVariantValue4.text = question.variants[3].translate
 
                         layoutAnswer1.setOnClickListener {
-                            val selectedWord = firstQuestion.variants[0]
-                            myViewModel?.checkAnswer(firstQuestion.questionId, selectedWord.wordId)
+                            myViewModel?.checkAnswer(
+                                question.questionId,
+                                question.variants[0].wordId
+                            )
 
                         }
                         layoutAnswer2.setOnClickListener {
-                            val selectedWord = firstQuestion.variants[1]
-                                myViewModel?.checkAnswer(firstQuestion.questionId, selectedWord.wordId)
+                            myViewModel?.checkAnswer(
+                                question.questionId,
+                                question.variants[1].wordId
+                            )
 
                         }
                         layoutAnswer3.setOnClickListener {
-                            val selectedWord = firstQuestion.variants[2]
-                                myViewModel?.checkAnswer(firstQuestion.questionId, selectedWord.wordId)
+                            myViewModel?.checkAnswer(
+                                question.questionId,
+                                question.variants[2].wordId
+                            )
 
                         }
                         layoutAnswer4.setOnClickListener {
-                            val selectedWord = firstQuestion.variants[3]
-                                myViewModel?.checkAnswer(firstQuestion.questionId, selectedWord.wordId)
+                            myViewModel?.checkAnswer(
+                                question.questionId,
+                                question.variants[3].wordId
+                            )
 
                         }
                     }
                 }
             }
-
-            private fun markAnswerCorrect(
-                layoutAnswer: LinearLayout,
-                tvVariantNumber: TextView,
-                tvVariantValue: TextView,
-            ) {
-
-                layoutAnswer.background = ContextCompat.getDrawable(
-                    this@MainActivity,
-                    R.drawable.shape_rounded_containers_correct
-                )
-
-                tvVariantNumber.background = ContextCompat.getDrawable(
-                    this@MainActivity,
-                    R.drawable.shape_rounded_variants_correct
-                )
-
-                tvVariantNumber.setTextColor(
-                    ContextCompat.getColor(
-                        this@MainActivity,
-                        R.color.white
-                    )
-                )
-
-                tvVariantValue.setTextColor(
-                    ContextCompat.getColor(
-                        this@MainActivity,
-                        R.color.correctAnswerColor
-                    )
-                )
-
-            }
-
-            private fun markAnswerWrong(
-                layoutAnswer: LinearLayout,
-                tvVariantNumber: TextView,
-                tvVariantValue: TextView,
-            ) {
-
-
-                layoutAnswer.background = ContextCompat.getDrawable(
-                    this@MainActivity,
-                    R.drawable.shape_rounded_containers_wrong
-                )
-
-                tvVariantNumber.background = ContextCompat.getDrawable(
-                    this@MainActivity,
-                    R.drawable.shape_rounded_variants_wrong
-                )
-
-                tvVariantNumber.setTextColor(
-                    ContextCompat.getColor(
-                        this@MainActivity,
-                        R.color.white
-                    )
-                )
-
-                tvVariantValue.setTextColor(
-                    ContextCompat.getColor(
-                        this@MainActivity,
-                        R.color.wrongAnswerColor
-                    )
-                )
-
-
-                binding.btnContinue.setTextColor(
-                    ContextCompat.getColor(
-                        this@MainActivity,
-                        R.color.wrongAnswerColor
-                    )
-                )
-
-                binding.layoutResult.isVisible = true
-            }
-
-            private fun markAnswerNeutral(
-                layoutAnswer: LinearLayout,
-                tvVariantNumber: TextView,
-                tvVariantValue: TextView,
-            ) {
-
-                layoutAnswer.background = ContextCompat.getDrawable(
-                    this@MainActivity,
-                    R.drawable.shape_rounded_containers
-                )
-
-                tvVariantValue.setTextColor(
-                    ContextCompat.getColor(
-                        this@MainActivity,
-                        R.color.textVariantsColor
-                    )
-                )
-
-                tvVariantNumber.apply {
-                    background = ContextCompat.getDrawable(
-                        this@MainActivity,
-                        R.drawable.shape_rounded_variants
-                    )
-                    setTextColor(
-                        ContextCompat.getColor(
-                            this@MainActivity,
-                            R.color.textVariantsColor
-                        )
-                    )
-                }
-            }
-
-            private fun showResultMessage(isCorrect: Boolean) {
-                val color: Int
-                val messageText: String
-                val resultIconResource: Int
-
-                if (isCorrect) {
-                    color = ContextCompat.getColor(this, R.color.correctAnswerColor)
-                    resultIconResource = R.drawable.ic_correct
-                    messageText = getResources().getString(R.string.title_correct)
-                } else {
-                    color = ContextCompat.getColor(this, R.color.wrongAnswerColor)
-                    resultIconResource = R.drawable.ic_wrong
-                    messageText = getResources().getString(R.string.title_wrong)
-                }
-
-
-
-                with(binding) {
-                    btnSkip.isVisible = false
-                    layoutResult.isVisible = true
-                    btnContinue.setTextColor(color)
-                    layoutResult.setBackgroundColor(color)
-                    tvResultMessage.text = messageText
-                    ivResultIcon.setImageResource(resultIconResource)
-                }
-
-            }
         }
+    }
+    private fun showNextQuestion() {
+        myViewModel?.getNextQuestion()
+    }
+
+    private fun markAnswerCorrect(
+        layoutAnswer: LinearLayout,
+        tvVariantNumber: TextView,
+        tvVariantValue: TextView,
+    ) {
+
+        layoutAnswer.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_correct
+        )
+
+        tvVariantNumber.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_variants_correct
+        )
+
+        tvVariantNumber.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.white
+            )
+        )
+
+        tvVariantValue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+    }
+
+    private fun markAnswerWrong(
+        layoutAnswer: LinearLayout,
+        tvVariantNumber: TextView,
+        tvVariantValue: TextView,
+    ) {
+
+
+        layoutAnswer.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_wrong
+        )
+
+        tvVariantNumber.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_variants_wrong
+        )
+
+        tvVariantNumber.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.white
+            )
+        )
+
+        tvVariantValue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+
+        binding.btnContinue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.layoutResult.isVisible = true
+    }
+
+    private fun markAnswerNeutral(
+        layoutAnswer: LinearLayout,
+        tvVariantNumber: TextView,
+        tvVariantValue: TextView,
+    ) {
+
+        layoutAnswer.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers
+        )
+
+        tvVariantValue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.textVariantsColor
+            )
+        )
+
+        tvVariantNumber.apply {
+            background = ContextCompat.getDrawable(
+                this@MainActivity,
+                R.drawable.shape_rounded_variants
+            )
+            setTextColor(
+                ContextCompat.getColor(
+                    this@MainActivity,
+                    R.color.textVariantsColor
+                )
+            )
+        }
+    }
+
+    private fun showResultMessage(isCorrect: Boolean) {
+        val color: Int
+        val messageText: String
+        val resultIconResource: Int
+
+        if (isCorrect) {
+            color = ContextCompat.getColor(this, R.color.correctAnswerColor)
+            resultIconResource = R.drawable.ic_correct
+            messageText = getResources().getString(R.string.title_correct)
+        } else {
+            color = ContextCompat.getColor(this, R.color.wrongAnswerColor)
+            resultIconResource = R.drawable.ic_wrong
+            messageText = getResources().getString(R.string.title_wrong)
+        }
+
+
+
+        with(binding) {
+            btnSkip.isVisible = false
+            layoutResult.isVisible = true
+            btnContinue.setTextColor(color)
+            layoutResult.setBackgroundColor(color)
+            tvResultMessage.text = messageText
+            ivResultIcon.setImageResource(resultIconResource)
+        }
+
+    }
+}
--- app/src/main/java/com/example/newapp/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/newapp/presentation/MainViewModel.kt
@@ -3,15 +3,20 @@ package com.example.newapp.presentation
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.newapp.domain.QuestionModel
 import com.example.newapp.domain.QuizeInteractor
 
 class MainViewModel(private val trainer: QuizeInteractor) : ViewModel() {
+
+    private val _question = MutableLiveData<QuestionModel>()
+    val question: LiveData<QuestionModel> = _question
+    fun getNextQuestion() {
+        val result = trainer.getNextQuestion()
+        _question.postValue(result)
+    }
+
     private val _answer = MutableLiveData<Boolean>()
     val answer: LiveData<Boolean> = _answer
-
-
-
-
     fun checkAnswer(questionId: Int, wordId: Int) {
         val result = trainer.checkAnswer(questionId, wordId)
         _answer.postValue(result)
